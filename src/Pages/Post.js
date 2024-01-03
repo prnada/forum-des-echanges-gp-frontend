f8d3c6a4b66251d4a456b70f00bbc734ab36ea80
prnada <elkhaldinada05@gmail.com>
2024-01-04 00:28:16 +0100

    delete post frontend

--- src/Pages/Post.js
+++ src/Pages/Post.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
+import DeleteConfirmation from '../components/DeleteConfirmation';
+import axios from 'axios';
 
 const PostList = () =>
 {
+
     //Traitement dyal les postes (fetch)
     const [posts, setPosts] = useState([]);
 
@@ -24,8 +27,10 @@ const PostList = () =>
         fetchPosts();
     }, []);
 
+  
     return (
-        <div>
+        <div >
+           
             {posts.map((post) => (
                 <Post
                     postId={post._id}
@@ -42,8 +47,35 @@ const PostList = () =>
 export default PostList;
 
 const Post = ({ title, date, creator, comments, category, postId }) => {
+
+ const [showModel, setShowModel] = useState(false);
+ const [itemToDelete, setItemToDelete] = useState(null);
+  function showDeleteConfirmation(id) {
+    setShowModel(true);
+    setItemToDelete(id);
+  }
+
+  function closeDeleteConfirmation() {
+    setShowModel(false);
+  }
+
+  const deleteConfirmHandler = (id) => {
+    axios.delete(`http://localhost:3001/deletePost/${id}`)
+      .then(res => {
+        console.log(res);
+        window.location.reload();
+      })
+      .catch(err => console.log(err));
+  };
     return (
       <div className="bg-white text-gray-800 rounded-lg p-4 my-4 flex flex-col md:flex-row justify-between items-center">
+         <DeleteConfirmation
+        showModel={showModel}
+        title="Delete Confirmation"
+        body="Are you sure to delete the item?"
+        closeDeleteConfirmation={closeDeleteConfirmation}
+        deleteConfirmHandler={() => deleteConfirmHandler(itemToDelete)}
+      />
         <div className="flex-1">
           <h3 className="text-xl font-semibold">{title}</h3>
           <p className="text-sm">Creator: {creator} | Date: {date}</p>
@@ -62,6 +94,12 @@ const Post = ({ title, date, creator, comments, category, postId }) => {
             Update
           </Link>
         </button>
+        <button >
+        <a href="#" onClick={() => showDeleteConfirmation(postId)} className="btn btn-danger" role="menuitem">Remove</a>
+        </button>
+        
+                          
+                  
       </div>
     );
 };
